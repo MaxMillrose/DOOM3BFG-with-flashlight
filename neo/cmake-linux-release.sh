689ee1b61d919a7204d3fd75dadc2d6053dcca37
Max Millrose <max.millrose@mailbox.org>
2021-11-10 00:04:24 +0100

    Updated to reflect the new script (complete.sh)

--- neo/cmake-linux-release.sh
+++ neo/cmake-linux-release.sh
@@ -1,7 +1,7 @@
 clear
 echo --------------------------------------------------------------------------------
 COLUMNS=$(tput cols) 
-title="DOOM BFA EDITION" 
+title="DOOM BFA EDITION with classic flashlight mod" 
 printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
 COLUMNS=$(tput cols) 
 title="UNIX MakeFile GENERATION FOR x86 - x64" 
@@ -10,15 +10,13 @@ COLUMNS=$(tput cols)
 title="RELEASE BUILD" 
 printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
 echo --------------------------------------------------------------------------------
-cd ..
-rm -rf build > /dev/null 2>&1
+rm -rfv build
 mkdir build > /dev/null 2>&1
 cd build 
 echo Generating files
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DONATIVE=ON -DSDL2=ON ../neo > log.txt ||(
-echo ERROR Generating files
+echo ERROR Generating files not removing stale build dir
 cd ..
-rmdir /Q /S build
 #read -rsp $'Press any key to continue...\n' -n1 key
 )
 #read -rsp $'Press any key to continue...\n' -n1 key

77bf718440e638efbe38fd09accd171c90631fab
Max Millrose <max.millrose@mailbox.org>
2021-11-11 15:10:04 +0100

    Added copying of files and optional game-launch

--- complete.sh
+++ complete.sh
@@ -5,24 +5,24 @@
 # Default choice is RELEASE 
 # of interest could be RETAIL (includes C++-11 standard) or DEBUG 
 
-TYPE=3
 
 
-
-echo $TYPE
 echo "Please enter the type of build you want: \n"
 echo "Choices are:  \n"
-echo "1. RETAIL \n"
+echo "1. RELEASE (picked by default) \n"
 echo "2. DEBUG \n"
-echo "3. RELEASE \n"
+echo "3. RETAIL \n"
 echo "For release and debug without intrinsic functions (no simd) please calll the scripts directly \n"
 
 read -p "Enter choice [i.e DEBUG or 2] or press enter for RELEASE " TYPE 
 
+read -p "Enter choice [i.e a,B,%,22] to STOP after copy or press enter to start game " PLAY 
+
+
 echo "Calling cmake-script for $TYPE"
 
 case "TYPE" in
-    "1" | "RETAIL" )
+    "3" | "RETAIL" )
         sh neo/cmake-linux-retail.sh 
         cd build ;;
 	"2" | "DEBUG" )
@@ -35,9 +35,19 @@ esac
  
 
 make -j17
+cd ../ 
 
-echo "Now we need to copy the files to our Doom3-BFG directory"
-echo "End line"
+echo "\n Now we need to copy the files to our Doom3-BFG directory \n"
+sleep 3 
+
+cp -va base/* test_D3BFG/base/
+cp -v build/DoomBFA test_D3BFG/DoomBFA
+
+if [ -z ${PLAY} ]; then 
+    cd test_D3BFG/ && ./DoomBFA;  
+else
+  echo "Stoping script here"
+fi
 	
 
 
